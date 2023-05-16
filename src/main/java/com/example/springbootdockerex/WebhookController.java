10c6ae7f39a41f5b0fa62b6543288e49eedcc3f7
김영근 <yaiba713@gmail.com>
2023-05-16 15:45:46 +0900

    feat: send-webhook

--- src/main/java/com/example/springbootdockerex/WebhookController.java
+++ src/main/java/com/example/springbootdockerex/WebhookController.java
@@ -5,14 +5,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class WebhookController {
 
-    @PostMapping("/send-webhook")
+    @RequestMapping(value = { "/send-webhook", "/send-webhook/" }, method = { RequestMethod.GET, RequestMethod.POST })
     public String sendWebhook() {
         String webhookUrl = "https://discord.com/api/webhooks/1090549186040184913/_nVKm8TfNkMjjZDz2lgNqbVQBmciLcgii6trO7nXIEz7LAtMHVRBXkSVwoSd-nYRGxzU";
         String message = "Hello, world!";
