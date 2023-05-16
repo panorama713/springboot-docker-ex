af89e3e294226e0f6b2aeb831a53f89f32d9c07c
김영근 <yaiba713@gmail.com>
2023-05-16 15:49:27 +0900

    feat: send-webhook

--- src/main/java/com/example/springbootdockerex/WebhookController.java
+++ src/main/java/com/example/springbootdockerex/WebhookController.java
@@ -8,18 +8,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
+
 @RestController
 public class WebhookController {
 
     @RequestMapping(value = { "/send-webhook", "/send-webhook/" }, method = { RequestMethod.GET, RequestMethod.POST })
     public String sendWebhook() {
         String webhookUrl = "https://discord.com/api/webhooks/1090549186040184913/_nVKm8TfNkMjjZDz2lgNqbVQBmciLcgii6trO7nXIEz7LAtMHVRBXkSVwoSd-nYRGxzU";
-        String message = "Hello, world!";
+        String message = LocalDate.now().toString();
+        String name = "저는 아직 취업하기는 글렀습니다."; // 원하는 이름으로 변경
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        String jsonPayload = "{\"content\": \"" + message + "\"}";
+        String jsonPayload = "{\"content\": \"" + message + "\", \"username\": \"" + name + "\"}";
 
         HttpEntity<String> requestEntity = new HttpEntity<>(jsonPayload, headers);
 
