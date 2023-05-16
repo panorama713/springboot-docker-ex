711942dfff36e2357ae849d4760eb0d66542ba2c
김영근 <yaiba713@gmail.com>
2023-05-16 15:13:24 +0900

    feat: send-webhook

--- src/main/java/com/example/springbootdockerex/HelloWorldController.java
+++ src/main/java/com/example/springbootdockerex/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.example.springbootdockerex;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.http.HttpHeaders.DATE;
 
 /**
  * packageName : com.example.springbootdockerex
@@ -19,5 +23,18 @@ public class HelloWorldController {
     @GetMapping("/")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/send-webhook")
+    public String sendWebhook() {
+        String webhookUrl = "https://discord.com/api/webhooks/1090549186040184913/_nVKm8TfNkMjjZDz2lgNqbVQBmciLcgii6trO7nXIEz7LAtMHVRBXkSVwoSd-nYRGxzU";
+        String hostname = System.getenv(DATE);
+
+        String jsonPayload = String.format("{\"content\": \"Hello, world!\", \"name\": \"%s\"}", hostname);
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForObject(webhookUrl, jsonPayload, String.class);
+
+        return "Webhook sent successfully";
     }
 }
