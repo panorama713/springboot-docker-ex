d76c7c0f495440f47286c9a1d17f8b55b1ab2b1e
김영근 <yaiba713@gmail.com>
2023-05-16 15:41:07 +0900

    feat: send-webhook

--- src/main/java/com/example/springbootdockerex/WebhookController.java
+++ src/main/java/com/example/springbootdockerex/WebhookController.java
@@ -1,10 +1,13 @@
 package com.example.springbootdockerex;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 @RestController
 public class WebhookController {
@@ -12,16 +15,22 @@ public class WebhookController {
     @PostMapping("/send-webhook")
     public String sendWebhook() {
         String webhookUrl = "https://discord.com/api/webhooks/1090549186040184913/_nVKm8TfNkMjjZDz2lgNqbVQBmciLcgii6trO7nXIEz7LAtMHVRBXkSVwoSd-nYRGxzU";
+        String message = "Hello, world!";
 
-        LocalDate currentDate = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String currentDateString = currentDate.format(formatter);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
-        String jsonPayload = String.format("{\"content\": \"Today is %s\", \"name\": \"%s\"}", currentDateString, System.getenv("HOSTNAME"));
+        String jsonPayload = "{\"content\": \"" + message + "\"}";
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(jsonPayload, headers);
 
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.postForObject(webhookUrl, jsonPayload, String.class);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(webhookUrl, HttpMethod.POST, requestEntity, String.class);
 
-        return "Webhook sent successfully";
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return "Webhook sent successfully";
+        } else {
+            return "Failed to send webhook. Response code: " + responseEntity.getStatusCodeValue();
+        }
     }
 }
